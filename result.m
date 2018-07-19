72ca783b784a5fcd3d8753ed3ed9520188ba135c
Gaurav <32817238+GauravBindal@users.noreply.github.com>
2018-07-19 22:14:42 +0530

    Delete result.m

--- result.m
+++
@@ -1,9 +0,0 @@
-% no on bit
-xresponsetime=[10,20,30,50,100,200];
-% response time
-yresponsetime=[0.3405,0.3363,0.3150,0.3860,0.4539]
-% Detection ratio
-zdetectionratio=[8/20,12/20,13/20,20/20,20/20,19/20]
-% Misdetection ratio
-dmisdetection=[12/20,8/20,7/20,0/20,0/20,1/20]
-
